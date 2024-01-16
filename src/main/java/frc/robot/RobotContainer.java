88387cefb8f6ec27bf29babaf7e9c702073bf7cd
Robotic-Racer14a <55024407+Robotic-Racer14a@users.noreply.github.com>
2024-01-15 21:26:06 -0500

    Smoothed Out Drive

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,22 +13,30 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.generated.TunerConstants;
+import frc.robot.nerdyfiles.utilities.Utilities;
 
 public class RobotContainer {
   private double MaxSpeed = 6; // 6 meters per second desired top speed
   private double MaxAngularRate = 1.5 * Math.PI; // 3/4 of a rotation per second max angular velocity
+  private double driveAdjustment = 1;
+  private double driveDeadband = 0.1;
+  private double angularDeadband = 0.1;
+  private double driveScale = 5;
 
   /* Setting up bindings for necessary control of the swerve drive platform */
   private final CommandXboxController joystick = new CommandXboxController(0); // My joystick
   public final CommandSwerveDrivetrain drivetrain = TunerConstants.DriveTrain; // My drivetrain
 
-  private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
-      .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+  private final SwerveRequest.FieldCentric driveFieldCentric = new SwerveRequest.FieldCentric()
+      .withRotationalDeadband(MaxAngularRate * angularDeadband) // Add a 10% deadband
       .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // I want field-centric
                                                                // driving in open loop
   private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
   private final SwerveRequest.RobotCentric forwardStraight = new SwerveRequest.RobotCentric().withDriveRequestType(DriveRequestType.OpenLoopVoltage);
   private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
+  private final SwerveRequest.FieldCentricFacingAngle driveFacingAngle = new SwerveRequest.FieldCentricFacingAngle()
+    .withRotationalDeadband(MaxAngularRate * angularDeadband) // Add a 10% deadband
+    .withDriveRequestType(DriveRequestType.OpenLoopVoltage); 
 
 
   /* Path follower */
@@ -37,19 +45,21 @@ public class RobotContainer {
   private final Telemetry logger = new Telemetry(MaxSpeed);
 
   private void configureBindings() {
-    joystick.x().whileTrue(new InstantCommand(() -> setMaxSpeed(2)));
+    joystick.back().whileTrue(new InstantCommand(() -> setMaxSpeed(driveScale))).onFalse(new InstantCommand(() -> setMaxSpeed(1)));
     
     drivetrain.setDefaultCommand( // Drivetrain will execute this command periodically
     
-        drivetrain.applyRequest(() -> drive.withVelocityX(-joystick.getLeftY() * MaxSpeed) // Drive forward with
-                                                                                           // negative Y (forward)
-            .withVelocityY(-joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
+        drivetrain.applyRequest(() -> driveFieldCentric.withVelocityX(Utilities.deadband(-joystick.getLeftY(), driveDeadband) * (MaxSpeed/driveAdjustment)) // Drive forward with negative Y (forward)
+            .withVelocityY(Utilities.deadband(-joystick.getLeftX(), driveDeadband) * (MaxSpeed/driveAdjustment)) // Drive left with negative X (left)
             .withRotationalRate(joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
         ).ignoringDisable(true));
 
     joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
-    joystick.b().whileTrue(drivetrain
-        .applyRequest(() -> point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))));
+    joystick.b().whileTrue(drivetrain.applyRequest(() -> point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))));
+    joystick.rightBumper().onTrue(drivetrain.applyRequest(() -> driveFacingAngle.withTargetDirection(new Rotation2d().fromDegrees(60))
+      .withVelocityX(Utilities.deadband(-joystick.getLeftY(), driveDeadband) * (MaxSpeed/driveAdjustment)) // Drive forward with negative Y (forward)
+      .withVelocityY(Utilities.deadband(-joystick.getLeftX(), driveDeadband) * (MaxSpeed/driveAdjustment)) // Drive left with negative X (left)
+      ).ignoringDisable(true));
 
     // reset the field-centric heading on left bumper press
     joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldRelative()));
@@ -63,7 +73,7 @@ public class RobotContainer {
     joystick.pov(180).whileTrue(drivetrain.applyRequest(() -> forwardStraight.withVelocityX(-0.5).withVelocityY(0)));
   }
   public void setMaxSpeed(double speed) {
-    MaxSpeed = speed;
+    driveAdjustment = speed;
   }
 
   public RobotContainer() {
---
+++ src/main/java/frc/robot/nerdyfiles/utilities/CTREUtils.java
@@ -0,0 +1,40 @@
+package frc.robot.nerdyfiles.utilities;
+
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+
+public class CTREUtils {
+
+  /**
+   * Creates a default limit configuration. This is base the current limit
+   * configuration we use on our motors to prevent overheating the motors
+   * in catastrophic scenerios.
+   */
+  public static CurrentLimitsConfigs setDefaultCurrentLimit() {
+    //TODO: Valdiate with documentation
+    CurrentLimitsConfigs defaultCurrentLimit = new CurrentLimitsConfigs();
+    defaultCurrentLimit.SupplyCurrentLimit = 40.0;
+    defaultCurrentLimit.SupplyCurrentThreshold = 50.0;
+    defaultCurrentLimit.SupplyTimeThreshold = 1.0;
+    defaultCurrentLimit.SupplyCurrentLimitEnable = true;
+
+    defaultCurrentLimit.StatorCurrentLimit = 50.0;
+    defaultCurrentLimit.StatorCurrentLimitEnable = true;
+    return defaultCurrentLimit;
+  }
+
+
+  public static CurrentLimitsConfigs setLowCurrentLimit() {
+    //TODO: Valdiate with documentation
+    CurrentLimitsConfigs lowCurrentLimit = new CurrentLimitsConfigs();
+    lowCurrentLimit.SupplyCurrentLimit = 30.0;
+    lowCurrentLimit.SupplyCurrentThreshold = 30.0;
+    lowCurrentLimit.SupplyTimeThreshold = 1.0;
+    lowCurrentLimit.SupplyCurrentLimitEnable = true;
+
+    lowCurrentLimit.StatorCurrentLimit = 30.0;
+    lowCurrentLimit.StatorCurrentLimitEnable = true;
+    return lowCurrentLimit;
+  }
+
+
+}
